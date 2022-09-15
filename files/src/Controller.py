7846338121f922c56c93e2418fa0a1fe7db0902e
5f-0 <No mail>
2022-09-15 17:20:11 +0200

    Fix intendation

--- files/src/Controller.py
+++ files/src/Controller.py
@@ -7,20 +7,20 @@ class Controller():
     def __init__(self) -> None:
         self.startTime = time.time()
 
-        def printHeader(self, path):
-            print("################################################################################")
-            print("")
-            print("Package Name by 5f0")
-            print("Insert description here...")
-            print("")
-            print("Current working directory: " + os.getcwd())
-            print("")
-            print(" Datetime: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-            print("")
-            print("################################################################################")
+    def printHeader(self, path):
+        print("################################################################################")
+        print("")
+        print("Package Name by 5f0")
+        print("Insert description here...")
+        print("")
+        print("Current working directory: " + os.getcwd())
+        print("")
+        print(" Datetime: " + datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+        print("")
+        print("################################################################################")
 
-        def printExecutionTime(self):
-            end = time.time()
-            print("")
-            print("Execution Time: " + str(end-self.startTime)[0:8] + " sec")
-            print("")
+    def printExecutionTime(self):
+        end = time.time()
+        print("")
+        print("Execution Time: " + str(end-self.startTime)[0:8] + " sec")
+        print("")
